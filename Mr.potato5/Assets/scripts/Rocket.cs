6d4c67e54d1168d97dddb246c611790e6fc794cc
wuchengdong-123 <2543447916@qq.com>
2021-05-30 21:28:38 +0800

    第五次实验

---
+++ Mr.potato5/Assets/scripts/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject explosion;
+    void Start()
+    {
+        Destroy(gameObject,2);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        float rotationZ = Random.Range(0, 360);
+        if(collision.tag != "Player")
+        {
+            Instantiate(explosion, transform.position, Quaternion.Euler(new Vector3(0, 0, rotationZ)));
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
